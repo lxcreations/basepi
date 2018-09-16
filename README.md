8efec622a083b7d97616c7690fa089ba18b5f64f
lxcreations <larrychadcox@gmail.com>
2018-09-16 14:16:31 -0500

    initial files added

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # basepi
-Custom bash setting and basic applications used for Local Fields Ranch Raspberry Pi's
+Custom setting and basic applications used for Local Fields Ranch Raspberry Pi's
+
+This project was originally created for use on the Raspberry Pi's (RPi's) I use on my Egg Farm and Grow Op, Local Fields Ranch.
+With the support of other users, I have decided to make this repo a little more general and friendlier to new RPi users.
+
+Originally, this was only used for dotfiles (like .bashrc) on my Linux based systems and was enspired by [flipsidecreations/dotfiles] (https://github.com/flipsidecreations/dotfiles).
+
+This quickly became more than just a repo for dotfiles to include the setup of new RPis with dotfiles, basic applications and custom scripts. Feel free to contant me with any questions or suggestions you may have.
+
+*GIT is a prerequeset for this package to be use as intended.*
+
+To find out if you have git installed, RUN:
+```which git```
+
+To install git, RUN:
+```sudo apt-get install git```
+
+To install this repo, RUN:
+```git clone https://github.com/lxcreations/basepi.git```
+
+**By Default, this should only be used on a Rasberry Pi running Raspian**
+This repo is designed for the Raspberry Pi running Raspian. It should work on any Raspian flavor, but it is untested. With little modification, this should be able to be installed on any Debian based system like Ubuntu or KDE Neon. With deeper modification, this could be used on any Linux flavor like OpenSuse or CentOS.
+
+
+**install.sh** - install all dotfiles and software used on the Raspberry Pi in the users home directory, usually /home/pi
+- Install also creates a scripts directory (~/scripts) for you to place custom bash, python, and other scripts. This directory is also placed in the .bashrc as an executable path
+- Installs packages if needed
+	- git <<  used to get GIT packages/repos
+	- sendemail << send email via bash scripts
+	- python-pip << install python packages
+	- python-pip3 << install python3 packages
+	- sysstat << get system status information
+	- snmp << monitor with snmp protocol
+	- samba << file sharing with Windows, Mac and Linux
+		
+
+**uninstall.sh** - remove custom dotfiles and restore originals.
+- Does not remove installed packages or ~/scripts directory
---
+++ install.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+#when is this
+STAMP=$(date +%Y%m%d_%H%M%S)
+#where are we
+SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+#include the baspi config file
+source $SOURCEDIR/config/basepi.conf
+
+#create initial ID variable for OS
+ID="unknown"
+
+#include OS details
+source /etc/os-release
+
+#Install ONLY if we are running Raspian
+if [ "$ID" != "raspian" ]; then
+  echo "ERROR: This is only tested with Raspberry Pi's running Raspian."
+  echo "Install is exiting."
+  # as a bonus, make our script exit with the right error code.
+  exit ${1}
+fi
+
+#get the current hostname
+HOSTNAME=$(hostname)
+
+#create variable for needed installable apps
+APTINSTALLS=""
+
+#lets backup the original dot files to restore on uninstall
+#and install the basepi dot files
+for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+  if [ -e ~/$file ]; then
+    echo "Backup original dotfile: "$file
+    mv -f ~/$file{,.dtbak}
+  fi
+  echo "Install custom dotfiles: "$file
+  ln -s $PWD/$file ~/$file
+done
+
+echo "Dotfiles installed"
+echo ""
+
+#create the scripts directory
+if [ ! -d ~/scripts ]; then
+    mkdir ~/scripts
+    echo "~/scripts directory Created"
+fi
+
+#create the basepi local directory
+if [ ! -d ~/.basepi ]; then
+  mkdir ~/.basepi
+  echo "~/.basepi directory created"
+fi
+
+#logfiles for basepi
+if [ ! -d ~/.basepi/logs ]; then
+  mkdir ~/.basepi/logs
+  echo "~/.basepi/logs directory created"
+fi
+
+#email config for basepi 
+if [ -e ~/.basepi/sendemail.conf ]; then
+    cp $SOURCEDIR/config/sendemail.conf ~/.basepi/sendemail.conf
+    echo "~/.basepi/sendemail.conf file created"
+    echo "You must modify the sendemail configuration file"
+    echo "nano ~/.basepi/sendemail.conf"
+fi
+
+#if git is not installed, mark it for install
+if [ ! -e /usr/bin/git ]; then
+    APTINSTALLS="$APTINSTALLS git"
+fi
+
+#if pip is not installed, mark it for install
+if [ ! -e /usr/bin/pip ]; then
+    APTINSTALLS="$APTINSTALLS python-pip"
+fi
+
+#if pip3 is not installed, mark it for install
+if [ ! -e /usr/bin/pip3 ]; then
+    APTINSTALLS="$APTINSTALLS python3-pip"
+fi
+
+#if sendemail is not installed, mark it for install
+if [ ! -e /usr/bin/sendemail ]; then
+    APTINSTALLS="$APTINSTALLS sendemail"
+fi
+
+#if sysstat is not installed, mark it for install
+if [ ! -e /usr/bin/iostat ]; then
+    APTINSTALLS="$APTINSTALLS sysstat"
+fi
+
+#if snmp is not installed, mark it for install
+if [ ! -e /usr/sbin/snmpd ]; then
+    APTINSTALLS="$APTINSTALLS snmpd snmp"
+fi
+
+#if samba is not installed, mark it for install
+if [ ! -e /usr/sbin/samba ]; then
+    APTINSTALLS="$APTINSTALLS samba samba-common-bin"
+fi
+
+#install marked packages and log the install
+sudo apt-get update; sudo apt-get install -yq $APTINSTALLS > ~/.basepi/logs/aptgetoninstall_$STAMP.log 2>&1
+
+#install the update notice script in crontab
+#SEE notes/updatenotice.txt for details
+if [ -e /usr/bin/sendemail ]; then
+    echo "Install Update Notice in cron"
+    ( crontab -l | grep -v -F "$UPDATESCRIPT" ; echo "$UPDATECRON" ) | crontab -
+fi
+
+#display the current OS
+echo "Current OS Version"
+echo $PRETTY_NAME
+
+#inform of installed packages
+echo "Installed base packages"
+echo $APTINSTALLS
+
+#warn about changing the hostname from original
+if [ "$HOSTNAME" = "raspberrypi" ]; then
+  echo "NOTES:hostname.txt-Hostname is set as default, you may want to change it to a specific name."
+fi
+
+#suggest changes on system config files
+echo "NOTES:snmpd.txt-Modify /etc/snmp/snmpd.conf for best results of snmp monitoring"
+echo "NOTES:samba.txt-Modify /etc/samba/smb.conf for best results of file sharing"
+
+#inform about updating the currently used bash shell
+echo "To refresh the bash console, run command"
+echo "source ~/.bashrc"
