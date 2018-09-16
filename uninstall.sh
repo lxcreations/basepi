2e41fa29898f401f8e6566b4bfcae75bcba90670
lxcreations <larrychadcox@gmail.com>
2018-09-16 14:26:04 -0500

    initial files added

---
+++ uninstall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#when are we
+STAMP=$(date +%Y%m%d_%H%M%S)
+
+#where are we
+SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+#include the baspi config file
+source $SOURCEDIR/config/basepi.conf
+for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+    if [ -h ~/$file ]; then
+        rm -f ~/$file
+    fi
+    if [ -e ~/${file}.dtbak ]; then
+        mv -f ~/$file{.dtbak,}
+    fi
+done
+
+#remove cron entries
+( crontab -l | grep -v -F "$UPDATESCRIPT" ) | crontab -
+rm -rf ~/.basepi
+echo "Uninstalled"
+echo "To refresh the bash console, run command"
+echo "source ~/.bashrc"
