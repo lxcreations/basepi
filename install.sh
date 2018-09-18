3f148c5084f290800cdf9f78cdfdc2ef229696ad
lxcreations <larrychadcox@gmail.com>
2018-09-18 10:37:16 -0500

    total rebuild

--- install.sh
+++ install.sh
@@ -13,6 +13,19 @@ ID="unknown"
 #include OS details
 source /etc/os-release
 
+echo "BasePi install script."
+echo "BasePi should only be installed on a Raspberry Pi running Raspian"
+echo "Please review the README.md and text files in the notes directory for more details before installing."
+
+while true; do
+    read -p "Do you wish to install BasePi? [y/n]: " doinstall
+    case $doinstall in
+        [Yy]* ) echo "continue"; break;;
+        [Nn]* ) echo "exiting"; exit 0;;
+        * ) echo "Please answer y for yes or n for no.";;
+    esac
+done
+
 #Install ONLY if we are running Raspian
 if [ "$ID" != "raspbian" ]; then
   echo "ERROR: This is only tested with Raspberry Pi's running Raspian."
@@ -38,7 +51,8 @@ for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
   fi
   if [ ! -e ~/$file ]; then
     echo "Install custom dotfiles: "$file
-    ln -s $PWD/$file ~/$file
+    cp $file ~/.basepi
+    ln -s ~/.basepi/$file ~/$file
   fi
 done
 
@@ -61,6 +75,12 @@ fi
 if [ ! -d ~/.basepi/logs ]; then
   mkdir ~/.basepi/logs
   echo "~/.basepi/logs directory created"
+fi
+
+#logfiles for basepi
+if [ ! -d ~/.basepi/scripts ]; then
+  mkdir ~/.basepi/scripts
+  echo "~/.basepi/scripts directory created"
 fi
 
 #email config for basepi 
@@ -109,18 +129,56 @@ if [ ! -e /usr/sbin/samba ]; then
     echo "NOTES:samba.txt-Modify /etc/samba/smb.conf for best results of file sharing"
 fi
 
+echo "These packages need to be installed."
+echo $APTINSTALLS
+
+while true; do
+    read -p "Would you like to install these packages via apt-get? [y/n]: " doinstall
+    case $doinstall in
+        [Yy]* )
+            echo "installing";
+            echo
+            if [ "$APTINSTALLS" != "" ]; then
+              AGINSLOG=~/.basepi/logs/aptgetoninstall_$(date +%Y%m%d_%H%M%S).log
+              sudo apt-get update; sudo apt-get install -yq $APTINSTALLS > $AGINSLOG 2>&1
+              echo 
+              echo "====================================================================="
+              echo "Review $AGINSLOG for install details"
+              echo "====================================================================="
+              echo 
+            fi
+            break;;
+        [Nn]* ) echo "skipping"; break;;
+        * ) echo "Please answer y for yes or n for no.";;
+    esac
+done
 #install marked packages and log the install
-if [ "$APTINSTALLS" != "" ]; then
-  sudo apt-get update; sudo apt-get install -yq $APTINSTALLS > ~/.basepi/logs/aptgetoninstall_$(date +%Y%m%d_%H%M%S).log 2>&1
-  echo ""
-fi
+
 
 #install the update notice script in crontab
 #SEE notes/updatenotice.txt for details
-if [ -e /usr/bin/sendemail ]; then
-    echo "Install Update Notice in cron"
-    ( crontab -l | grep -v -F "$UPDATESCRIPT" ; echo "$UPDATECRON" ) | crontab -
-fi
+
+echo "The Update Notice checks for updates via apt-get and emails you the results."
+echo "It is installed as a cronjob to run once a week, Sunday at 1AM."
+echo "This only notifies you of updates, it does not install them."
+while true; do
+    read -p "Would you like to install the Update Notice cron job? [y/n]: " doinstall
+    case $doinstall in
+        [Yy]* )
+            echo "installing";
+            echo
+            if [ -e /usr/bin/sendemail ]; then
+                ( crontab -l | grep -v -F "$UPDATESCRIPT" ; echo "$UPDATECRON" ) | crontab -
+            else
+                echo "Sendemail package is missing. Cronjob will not be installed."
+                echo "RUN: sudo apt-get install sendemail to install"
+            fi
+            break;;
+        [Nn]* ) echo "skipping"; break;;
+        * ) echo "Please answer y for yes or n for no.";;
+    esac
+done
+
 
 #display the current OS
 echo "Current OS Version: "$PRETTY_NAME
@@ -141,3 +199,4 @@ fi
 #inform about updating the currently used bash shell
 echo "To refresh the bash console, run command"
 echo "source ~/.bashrc"
+source ~/.bashrc
